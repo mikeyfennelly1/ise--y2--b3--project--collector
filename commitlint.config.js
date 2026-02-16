a660c3792c28d7392c470733552f661ec47b88aa
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2026-02-16 15:03:55 +0000

    update settings to commit linting for longer commit msgs (120)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 120],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+  },
 };
