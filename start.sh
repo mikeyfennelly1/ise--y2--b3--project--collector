90b33fa25e7e9a736a0412abfc71a7fb76b0d517
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2026-02-20 14:43:54 +0000

    chore: add option to startup script that allows user to start app even if nats isn't healthy immediately

--- start.sh
+++ start.sh
@@ -20,14 +20,19 @@ SERVICE_NAME="collector"
 ################################################
 # NATS is healthy
 ################################################
-var_must_exist NATS_PORT NATS_HOST
+if [[ "$1" != "retry-nats" ]]; then
 
-nc  -z -v -w 2 "${NATS_HOST}" "${NATS_PORT}"
-if [ $? -eq 0 ]; then
-  echo "${SERVICE_NAME}: NATS is healthy on http://${NATS_HOST}:${NATS_PORT}"
+	var_must_exist NATS_PORT NATS_HOST
+
+	nc  -z -v -w 2 "${NATS_HOST}" "${NATS_PORT}"
+	if [ $? -eq 0 ]; then
+	  echo "${SERVICE_NAME}: NATS is healthy on http://${NATS_HOST}:${NATS_PORT}"
+	else
+	  echo "${SERVICE_NAME}: ERROR: NATS is unhealthy. Exiting..."
+	  exit 1
+	fi
 else
-  echo "${SERVICE_NAME}: ERROR: NATS is unhealthy. Exiting..."
-  exit 1
+	echo "script configured to run in retry-nats mode..."
 fi
 
 ################################################
